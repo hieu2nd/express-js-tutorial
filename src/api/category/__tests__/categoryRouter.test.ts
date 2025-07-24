fa690e8a31c95038e172f31290fc15272cc3cc30
Andy Nguyen <andynguyen@Andys-MacBook-Pro.local>
2025-07-24 15:23:25 +0700

    remove unit tests

--- src/api/category/__tests__/categoryRouter.test.ts
+++ src/api/category/__tests__/categoryRouter.test.ts
@@ -1,15 +1,1 @@
-import { StatusCodes } from "http-status-codes";
-import request from "supertest";
-
-import type { User } from "@/api/user/userModel";
-import { users } from "@/api/user/userRepository";
-import type { ServiceResponse } from "@/common/models/serviceResponse";
-import { app } from "@/server";
-
 describe("User API Endpoints", () => {});
-
-function compareUsers(mockUser: User, responseUser: User) {
-  if (!mockUser || !responseUser) {
-    throw new Error("Invalid test data: mockUser or responseUser is undefined");
-  }
-}
